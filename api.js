a1c9aba749b80d4b6d3acbdc15f3d7c35161582e
marjanamahii <marjanamahi777@gmail.com>
2021-09-02 05:28:39 +0600

    second commit

--- api.js
+++ api.js
@@ -1,11 +1,17 @@
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    console.log(searchText);
+    // console.log(searchText);
 
     searchField.value = '';
 
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
-    console.log(url);
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.docs));
 
+}
+const displaySearchResult = docs => {
+    console.log(docs)
 }
